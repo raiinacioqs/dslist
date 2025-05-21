5faa2995c94b53f5b965424f2cbe619622b81047
raiinacioqs <raiinacio380@gmail.com>
2025-05-21 17:38:38 -0300

    Projections

--- src/main/java/com/raiinacioqs/dslist/services/GameService.java
+++ src/main/java/com/raiinacioqs/dslist/services/GameService.java
@@ -9,9 +9,8 @@ import org.springframework.transaction.annotation.Transactional;
 import com.raiinacioqs.dslist.dto.GameDTO;
 import com.raiinacioqs.dslist.dto.GameMinDTO;
 import com.raiinacioqs.dslist.entities.Game;
+import com.raiinacioqs.dslist.projections.GameMinProjection;
 import com.raiinacioqs.dslist.repositories.GameRepository;
-
-
 
 @Service
 public class GameService {
@@ -28,6 +27,12 @@ public class GameService {
     @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
        List<Game> result =  gameRepository.findAll();
+       return result.stream().map(x -> new GameMinDTO(x)).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId) {
+       List<GameMinProjection> result =  gameRepository.searchByList(listId);
        return result.stream().map(x -> new GameMinDTO(x)).toList();
     }
 }
