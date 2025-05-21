e9bb034ad14cf5b6c132297e0c52e56b1fcd8079
raiinacioqs <raiinacio380@gmail.com>
2025-05-21 16:33:57 -0300

    Obter o game pelo id (GET Game by ID)

--- src/main/java/com/raiinacioqs/dslist/repositories/GameRepository.java
+++ src/main/java/com/raiinacioqs/dslist/repositories/GameRepository.java
@@ -1,9 +1,11 @@
 package com.raiinacioqs.dslist.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.raiinacioqs.dslist.entities.Game;
 
+@Repository
 public interface GameRepository extends JpaRepository<Game, Long>{
 
     
--- src/main/java/com/raiinacioqs/dslist/services/GameService.java
+++ src/main/java/com/raiinacioqs/dslist/services/GameService.java
@@ -4,17 +4,28 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.raiinacioqs.dslist.dto.GameDTO;
 import com.raiinacioqs.dslist.dto.GameMinDTO;
 import com.raiinacioqs.dslist.entities.Game;
 import com.raiinacioqs.dslist.repositories.GameRepository;
+
+
 
 @Service
 public class GameService {
 
     @Autowired
     private GameRepository gameRepository;
-    
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result); 
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
        List<Game> result =  gameRepository.findAll();
        return result.stream().map(x -> new GameMinDTO(x)).toList();
